1ebf49eed06e093c28687a899d3fa962e5f9e8d9
houchuyi <skyhou1728@gmail.com>
2025-12-29 14:28:17 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/xstudio-interactive.github.io/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
